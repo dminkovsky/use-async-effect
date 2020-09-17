92c81d910965abee8b88d1759ab09f1cbe4b081d
Dmitry Minkovsky <dminkovsky@gmail.com>
2020-09-17 09:57:28 -0400

    Ran prettier

--- index.js
+++ index.js
@@ -5,25 +5,28 @@ var React = require('react');
 function useAsyncEffect(effect, destroy, inputs) {
   var hasDestroy = typeof destroy === 'function';
 
-  React.useEffect(function () {
-    var result;
-    var mounted = true;
-    var maybePromise = effect(function () {
-      return mounted;
-    });
+  React.useEffect(
+    function() {
+      var result;
+      var mounted = true;
+      var maybePromise = effect(function() {
+        return mounted;
+      });
 
-    Promise.resolve(maybePromise).then(function (value) {
-      result = value;
-    });
+      Promise.resolve(maybePromise).then(function(value) {
+        result = value;
+      });
 
-    return function () {
-      mounted = false;
+      return function() {
+        mounted = false;
 
-      if (hasDestroy) {
-        destroy(result);
-      }
-    };
-  }, hasDestroy ? inputs : destroy);
+        if (hasDestroy) {
+          destroy(result);
+        }
+      };
+    },
+    hasDestroy ? inputs : destroy
+  );
 }
 
 module.exports = useAsyncEffect;
